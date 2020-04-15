5fc07a109451b3783eada9736176fe273486250c
paultanbunan <paul.tanbunan@gmail.com>
2020-04-15 09:41:13 +0700

    2 files modified - pola B

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  hasil = 0;
+  pola = "";
+  hasil_pola = "";
+  temp = "";
+  i;
+  k;
+  klikbutton(a, b){
+    this.hasil = Math.pow(a,b);
+    if(this.hasil % 2 == 0){
+      this.pola = "B";
+      for (this.i = b;this.i < this.hasil;this.i++){
+        this.temp = "";
+        for (this.k = 0; this.k < this.i;this.k++){
+          this.temp += "*";
+        }
+        document.getElementById("hasil").append( this.temp);
+        document.getElementById("hasil").insertAdjacentHTML('beforeend','<br>');
+      }
+    }
+    else{
+      this.pola = "A";
+      
+    }
+  };
 }
