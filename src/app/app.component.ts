caed3818cba6848b152054bbc44efb2cc257777e
paultanbunan <paul.tanbunan@gmail.com>
2020-04-15 10:12:09 +0700

    1 file modified - pola a & b

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,11 @@ export class AppComponent  {
   pola = "";
   hasil_pola = "";
   temp = "";
+  temp1= "o";
   i;
+  j;
   k;
+  counter = 1;
   klikbutton(a, b){
     this.hasil = Math.pow(a,b);
     this.temp = "";
@@ -33,19 +36,27 @@ export class AppComponent  {
       document.getElementById("hasil").innerHTML = this.temp;
       for(this.i = 0;this.i < b; this.i++){
         if (this.i == 0){
-          for (this.k = b; this.k >= 0;this.k--){
+          for (this.k = b; this.k > 0;this.k--){
             this.temp += "*";
           }
-          document.getElementById("hasil").append( this.temp);
+          document.getElementById("hasil").append( this.temp+ this.temp1);
           document.getElementById("hasil").insertAdjacentHTML('beforeend','<br>');
         }
         else{
           for (this.k = b-1; this.k >= 0;this.k--){
             this.temp = this.temp.substring(0, this.temp.length - 1);
-            document.getElementById("hasil").append( this.temp);
-            document.getElementById("hasil").insertAdjacentHTML('beforeend','<br>');
+            if(this.counter < a){
+              this.temp1 += "o"
+              this.counter++;
+              document.getElementById("hasil").append( this.temp+this.temp1);
+              document.getElementById("hasil").insertAdjacentHTML('beforeend','<br>');
+            }
+            else{
+              document.getElementById("hasil").append( this.temp);
+              document.getElementById("hasil").insertAdjacentHTML('beforeend','<br>');
             }
           }
+        }
       }
     }
   };
